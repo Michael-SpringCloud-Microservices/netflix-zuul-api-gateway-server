83f4a180d266ffe3d65a336cfa9ddee77e4d23dd
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2019-06-04 18:21:47 +0530

    The latest code changes

--- src/main/java/com/in28minutes/microservices/zuul/gatewayserver/ZuulLoggingFilter.java
+++ src/main/java/com/in28minutes/microservices/zuul/gatewayserver/ZuulLoggingFilter.java
@@ -18,7 +18,7 @@ import com.netflix.zuul.exception.ZuulException;
  *
  */
 @Component
-public class ZuulLiggingFilter extends ZuulFilter{
+public class ZuulLoggingFilter extends ZuulFilter{
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
